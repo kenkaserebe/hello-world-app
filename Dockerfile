91928a2cbc8c83aed9ed2d8defa6198284ca9f4e
kazaraal <araalkaz@gmail.com>
2026-03-28 15:43:58 +0000

    Added commit: Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.9-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY app.py .
+
+# You can set an environment variable to distiguish clouds in the response
+# ENV CLOUD=aws # will be overridden in Kubernetes manifest
+
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    # Optionally show which cloud (you can pass an env var)
+    cloud = os.environ.get('CLOUD', 'unknown')
+    return f"Hello from {cloud}!\n"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
